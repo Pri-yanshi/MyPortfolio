2d052e0c6c9be55eaf6a9aa50b67b97b539f948e
Pri-yanshi <priyanshimaurya008@gmail.com>
2023-10-31 09:54:23 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,23 @@ import Skill from './components/Skills/skill';
 import Work from './components/Works/work';
 import Contact from './components/Contact/contact';
 import Footer from './components/Footer/footer';
+import { HashRouter } from 'react-router-dom';
 
 
 const App = () => {
   return (
     <>
       <div className='App'>
+        <HashRouter>
         <Navbar/>
         <Intro/>
         <Skill/>
         <Work/>
         <Contact/>
         <Footer/>
+        </HashRouter>
       </div>
+      
     </>
   )
 }
